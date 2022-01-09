7cdc06171525ece0adfe21db2a30d07ccd4cb8d9
pointjunseo <pointjunseo@dgist.ac.kr>
2022-01-09 14:26:48 +0900

    first design

---
+++ app/src/main/java/com/example/clientapp/RankingAdapter.java
@@ -0,0 +1,59 @@
+package com.example.clientapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHolder> {
+
+    private ArrayList<RankingItem> RankingList;
+
+    @NonNull
+    @Override
+    public RankingAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ranking_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RankingAdapter.ViewHolder holder, int position) {
+        holder.onBind(RankingList.get(position));
+    }
+
+    public void setFriendList(ArrayList<RankingItem> list){
+        this.RankingList = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return RankingList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView name;
+        TextView ranking;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = (ImageView) itemView.findViewById(R.id.image);
+            name = (TextView) itemView.findViewById(R.id.name);
+            ranking = (TextView) itemView.findViewById(R.id.ranking);
+        }
+
+        void onBind(RankingItem item){
+            image.setImageResource(item.getImage());
+            name.setText(item.getName());
+            ranking.setText(item.getRanking());
+        }
+    }
+}
--- app/src/main/java/com/example/clientapp/findroom.java
+++ app/src/main/java/com/example/clientapp/findroom.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.BlockedNumberContract;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,6 +31,7 @@ public class findroom extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_findroom);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
