9248b17e8d7acfd24364b9d204d48820f535e65c
impooh121 <impooh121@kaist.ac.kr>
2022-01-10 16:50:00 +0900

    very small change

--- app/src/main/java/com/example/clientapp/MessageData.java
+++ app/src/main/java/com/example/clientapp/MessageData.java
@@ -4,12 +4,14 @@ import android.widget.Toast;
 // move,i,j,null
 // wall,i,j,horizontal
 public class MessageData {
-    String username,roomnumber,content,mtype,move,detail,win,lose;
+    String username,roomnumber,content,mtype,move,detail;
+    int win,lose;
     MessageData(String username,String roomnumber,String content,String detail,String move){
         this.username=username;
         this.roomnumber=roomnumber;
         this.content=content;
         this.detail=detail;
         this.move=move;
+        win=lose=0;
     }
 }
